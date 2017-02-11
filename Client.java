5b7acc79d14aa1a6961f91e6b6c85cba15396781
Carson Wood <carsnwd@gmail.com>
2017-02-11 13:20:52 -0500

    Refactored client
     added I/O to send a message to server so testing if multithread server works

--- Client.java
+++ Client.java
@@ -13,11 +13,21 @@ import java.util.Scanner;
  */
 public class Client
 {
+    private static final int port = 4446;
+
+    private static final String host = "localhost";
+
     //Output to server
     PrintWriter out;
+
     //Input from server
     Scanner conIn;
-    
+
+    //Input from user
+    Scanner in;
+
+    String[] tenLetWords = { "Strawberry", "California", "Everything", "Technology", "Volleyball", "Homecoming", "Relaxation", "Television", "University", "Cinderella" };
+
     /**
      * Creates a new instance of the client
      * @param args
@@ -39,31 +49,66 @@ public class Client
      */
     public void run() throws UnknownHostException, IOException
     {
-        System.out.println("Connecting to server...");
-        Socket connection = new Socket("localhost", 4446); //Connects to server
+        String choice; //To advance client and send next string
+        String msg; //Recieve messages from server
+
+        //Opens connection
+        Socket connection = openConnection();
+
+        //Gets I/O streams from server
         out = new PrintWriter(connection.getOutputStream());
         conIn = new Scanner(connection.getInputStream());
-        String msg = conIn.nextLine();
+        msg = conIn.nextLine();
         System.out.println(msg);
-        send("Strawberry");
-        send("California");
-        send("Everything");
-        send("Technology");
-        send("Volleyball");
-        send("Homecoming");
-        send("Relaxation");
-        send("Television");
-        send("University");
-        send("Cinderella");
+        in = new Scanner(System.in);
+
+        /*
+         * Loops through strings to reverse
+         */
+        for (String s : tenLetWords)
+        {
+            send(s);
+            System.out.println("Press 1 to continue to next string...");
+            choice = in.next();
+            while (!choice.equals("1"))
+            {
+                choice = in.next();
+            }
+
+        }
+        closeConnection(connection); //Closes the connection
+    }
+
+    /**
+     * Open the connection
+     * @return
+     * @throws UnknownHostException
+     * @throws IOException
+     */
+    private Socket openConnection() throws UnknownHostException, IOException
+    {
+        System.out.println("Connecting to server...");
+        Socket connection = new Socket(host, port); //Connects to server
+        return connection;
+    }
+
+    /**
+     * Closes the connection
+     * @param connection
+     * @throws IOException
+     */
+    private void closeConnection(Socket connection) throws IOException
+    {
+        System.out.println("Closing connection...");
         connection.close();
     }
-    
+
     /**
      * Sends a message to the server
      * with the string to reverse.
      * @param s
      */
-    public void send(String s)
+    private void send(String s)
     {
         System.out.println("You sent " + s + ", sending message...");
         out.println(s);
